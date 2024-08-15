2fa922dd4c7eacb882d78350ee29c05a4165dd39
Terminator19 <48512516+Terminator19@users.noreply.github.com>
2024-08-15 18:55:42 +0200

    pokračovanie nabuduce

--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -4,8 +4,20 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// Importuj PocketBase
+import PocketBase from 'pocketbase'
+
+// Inicializuj PocketBase
+const pb = new PocketBase('http://localhost:8090')
+
+// Môžeš použiť `pb` kdekoľvek vo svojej aplikácii
+// Napríklad: pb.collection('users').getList()
+
 const app = createApp(App)
 
 app.use(createPinia())
+
+// Pridaj PocketBase do globálnych vlastností aplikácie
+app.config.globalProperties.$pb = pb
 
 app.mount('#app')
